f1cd1727c4110eaa6cf4953458f9e354ac51ad07
FelixNihBous <felixlix604@gmail.com>
2025-03-25 13:17:32 +0700

    First Step of My Portofolio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingPage from './component/LandingPage.jsx'
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+export default App
